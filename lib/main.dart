df7c4636129ca76132c3ecbf266f52b0a88585c7
Srishyl <srishylkumar29@gmail.com>
2026-03-10 11:37:42 +0530

    Configure Firebase for Blinkit Clone

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'categories_page.dart';
 import 'cart_page.dart';
 import 'profile_page.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const BlinkitApp());
 }
 
